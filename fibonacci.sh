2ba8c9d16bfa45e97026f7bb9a4e17df16ae62bd
keitaromae <54606445+keitaromae@users.noreply.github.com>
2019-09-18 23:02:47 +0900

    Update fibonacci.sh

--- fibonacci.sh
+++ fibonacci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -n "Enter number of terms and press ENTER: "
+echo "Enter number of terms and press ENTER: "
 read N
 (( x=$N + 1 ))
 
@@ -9,12 +9,15 @@ b=1
 i=0
 
 echo "The fibonacci sequence is: "
+
 while [ $i -lt $x ]
 do
+
     ((sum= $a + $b ))
     a=$b
     b=$sum  
     echo "$a"
     ((i= $i + 1 ))
+
 done
 exit
